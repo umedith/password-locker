1913f6011854873897f086db15b291d203379705
umedith <umedith03@gmail.com>
2020-11-14 17:26:03 +0200

    added the function to save the account credentials

--- credentials.py
+++ credentials.py
@@ -9,6 +9,7 @@ class Account:
         self.account_userName = account_userName
         self.account_password = account_password
 
+
     def save_account(self):
         Account.account_list.append(self)
 
