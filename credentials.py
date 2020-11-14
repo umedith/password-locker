3808f5dca67ec631c31bd6ed395410e6d29b14a3
umedith <umedith03@gmail.com>
2020-11-14 17:28:13 +0200

    imported the user class in the user test file

--- credentials.py
+++ credentials.py
@@ -22,6 +22,7 @@ class Account:
     def delete_account(self):
         Account.account_list.remove(self)
 
+
     @classmethod
     def find_by_accountName(cls,accountname):
         for account in cls.account_list:
